7cd766cff24f996009e60a3ffb0bda689bb10a8c
Bilal Shuja <bilalshuja77@gmail.com>
2024-03-08 22:28:57 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TermsAndCondition from './Components/TermsAndCondition';
+
+function App() {
+  return (
+    <div className="App">
+     <TermsAndCondition/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TermsAndCondition.jsx
@@ -0,0 +1,304 @@
+import React from "react";
+import TandCImage from "./accept-terms-pana.svg";
+
+const TermsAndCondition = () => {
+  return (
+    <>
+      <div className="container p-5">
+        <h3
+          style={{ fontFamily: "Metropolis , -apple-system", color: "363D47" }}
+        >
+          Terms and Condition
+        </h3>
+        <hr />
+
+        <div className="row">
+          <div className="col-lg-8">
+            <p style={{ color: "#69707A" }}>
+              Welcome to Icate! These terms and conditions outline the rules and
+              regulations for the use of the Icate mobile application.
+            </p>
+
+            <p style={{ color: "#69707A" }}>
+              By accessing this mobile app, we assume you accept these terms and
+              conditions. Do not continue to use Icate if you do not agree to
+              all of the terms and conditions stated on this page.
+            </p>
+
+            <p style={{ color: "#69707A", lineHeight: "1.7em" }}>
+              The following terminology applies to these Terms and Conditions,
+              Privacy Statement, and Disclaimer Notice and all agreements:
+              "Client", "You", and "Your" refers to you, the person log on this
+              mobile app and compliant to the Company’s terms and conditions.
+              <span className="fw-bold">
+                "The Company", "Ourselves", "We", "Our", and "Us", refers to our
+                Company. "Party", "Parties", or "Us"
+              </span>
+              , refers to both the Client and ourselves. All terms refer to the
+              offer, acceptance, and consideration of payment necessary to
+              undertake the process of our assistance to the Client in the most
+              appropriate manner for the express purpose of meeting the Client’s
+              needs in respect of provision of the Company’s stated services, in
+              accordance with and subject to, prevailing law of the United
+              States. Any use of the above terminology or other words in the
+              singular, plural, capitalization, and/or he/she or they, are taken
+              as interchangeable and therefore as referring to same.
+            </p>
+
+            <hr />
+
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                Cookies
+              </h3>
+            </div>
+            <p style={{ color: "#69707A" }}>
+              We employ the use of cookies. By accessing Icate, you agreed to
+              use cookies in agreement with the Icate's Privacy Policy.
+            </p>
+            <p style={{ color: "#69707A" }}>
+              Most interactive websites use cookies to let us retrieve the
+              user’s details for each visit. Cookies are used by our website to
+              enable the functionality of certain areas to make it easier for
+              people visiting our website. Some of our affiliate/advertising
+              partners may also use cookies.
+            </p>
+
+            <hr />
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                License
+              </h3>
+            </div>
+            <p style={{ color: "#69707A" }}>
+              Unless otherwise stated, Icate and/or its licensors own the
+              intellectual property rights for all material on Icate. All
+              intellectual property rights are reserved. You may access this
+              from Icate for your own personal use subjected to restrictions set
+              in these terms and conditions.
+            </p>
+
+            <hr />
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                iFrames
+              </h3>
+            </div>
+
+            <p style={{ color: "#69707A" }}>
+              Without prior approval and written permission, you may not create
+              frames around our Webpages that alter in any way the visual
+              presentation or appearance of our Mobile App.
+            </p>
+
+            <hr />
+
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                Content Liability
+              </h3>
+            </div>
+
+            <p style={{ color: "#69707A" }}>
+              We shall not be hold responsible for any content that appears on
+              your Mobile App. You agree to protect and defend us against all
+              claims that is rising on your Mobile App. No link(s) should appear
+              on any Mobile App that may be interpreted as libelous, obscene or
+              criminal, or which infringes, otherwise violates, or advocates the
+              infringement or other violation of, any third party rights.
+            </p>
+
+            <hr />
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                Removal of links from our Mobile App
+              </h3>
+            </div>
+
+            <p style={{ color: "#69707A" }}>
+              If you find any link on our Mobile App that is offensive for any
+              reason, you are free to contact and inform us any moment. We will
+              consider requests to remove links but we are not obligated to or
+              so or to respond to you directly.
+            </p>
+
+            <p style={{ color: "#69707A" }}>
+              We do not ensure that the information on this Mobile App is
+              correct, we do not warrant its completeness or accuracy; nor do we
+              promise to ensure that the Mobile App remains available or that
+              the material on the Mobile App is kept up to date.
+            </p>
+
+            <hr />
+
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                Disclaimer
+              </h3>
+            </div>
+
+            <p style={{ color: "#69707A" }}>
+              To the maximum extent permitted by applicable law, we exclude all
+              representations, warranties, and conditions relating to our Mobile
+              App and the use of this Mobile App. Nothing in this disclaimer
+              will:
+            </p>
+
+            <ul style={{ color: "#69707A" }}>
+              <li>
+                limit or exclude our or your liability for death or personal
+                injury.
+              </li>
+              <li>
+                limit or exclude our or your liability for fraud or fraudulent
+                misrepresentation
+              </li>
+              <li>
+                limit any of our or your liabilities in any way that is not
+                permitted under applicable law
+              </li>
+              <li>
+                exclude any of our or your liabilities that may not be excluded
+                under applicable law
+              </li>
+            </ul>
+
+            <p style={{ color: "#69707A" }}>
+              The limitations and prohibitions of liability set in this Section
+              and elsewhere in this disclaimer: (a) are subject to the preceding
+              paragraph; and (b) govern all liabilities arising under the
+              disclaimer, including liabilities arising in contract, in tort and
+              for breach of statutory duty.
+            </p>
+
+            <p style={{ color: "#69707A" }}>
+              As long as the Mobile App and the information and services on the
+              Mobile App are provided free of charge, we will not be liable for
+              any loss or damage of any nature.
+            </p>
+
+            <hr />
+
+            <div className="cookie-section d-flex align-self-center">
+              <i
+                className="fa-solid fa-circle-arrow-right fs-3 me-2"
+                style={{ color: "#dc392d" }}
+              />
+              <h3
+                style={{
+                  fontFamily: "Metropolis , -apple-system",
+                  color: "363D47",
+                }}
+              >
+                Refund Policy
+              </h3>
+            </div>
+
+            <p style={{ color: "#69707A" }}>
+              <h6 style={{ color: "#69707A" }}>Coffee Purchases:</h6>
+              <ul>
+                <li>
+                  Contact us within 24 hours of receiving your order if you're
+                  unsatisfied with the quality. We may offer a replacement or
+                  refund.
+                </li>
+                <li>
+                  Refunds will be issued as store credit, cryptocurrency, or
+                  through the original payment method.
+                </li>
+              </ul>
+
+              <h6 style={{ color: "#69707A" }}>Crypto Rewards:</h6>
+              <ul>
+                <li>
+                  Crypto rewards are non-refundable and cannot be exchanged for
+                  cash.
+                </li>
+              </ul>
+
+              <h6 style={{ color: "#69707A" }}>Cancellation:</h6>
+
+              <ul>
+                <li>
+                  Orders cannot be cancelled once confirmed and processed unless
+                  under exceptional circumstances.
+                </li>
+              </ul>
+
+
+              <h6 style={{ color: "#69707A" }}>Contact Us:</h6>
+
+                    <ul>
+                    <li>
+                    For assistance with refunds or cancellations, reach out to our customer support team.
+                    </li>
+                    </ul>
+
+
+            </p>
+          </div>
+
+          <div className="col-lg-4">
+            <img src={TandCImage} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TermsAndCondition;
